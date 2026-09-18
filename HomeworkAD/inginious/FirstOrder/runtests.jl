ca5e2d04b8e7db1649b929986d0c386dedafc022
Benoît Legat <benoit.legat@gmail.com>
2026-09-18 09:16:04 +0200

    Remove homework

--- HomeworkAD/inginious/FirstOrder/runtests.jl
+++
@@ -1,8 +0,0 @@
-include("test.jl")
-
-# Reference implementation we test against
-include("forward.jl")
-
-## First order
-include("reverse_vectorized.jl")
-run_gradient_tests(Forward.gradient, VectReverse.gradient)
--- HomeworkAD/inginious/SecondOrder/runtests.jl
+++
@@ -1,8 +0,0 @@
-include("test.jl")
-
-# Reference implementation we test against
-include("forward.jl")
-
-## Second order
-include("reverse_vectorized.jl")
-run_gradient_tests(Forward.hessian, VectReverse.hessian, hessian = true)
